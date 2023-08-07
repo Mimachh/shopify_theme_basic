e464e9a94ac38909f848f1920cd95c8d825c9e71
unknown <karl.mullr@gmail.com>
2023-08-07 11:20:36 +0200

    first commit

---
+++ assets/application.js
@@ -0,0 +1,89 @@
+// Put your application javascript here
+document.addEventListener('DOMContentLoaded', function() {
+    // Menu Nav Dropdown
+    const dropdownButtons = document.querySelectorAll('.dropdown-button');
+    const dropdownContents = document.querySelectorAll('.dropdown-content');
+  
+    dropdownButtons.forEach((button) => {
+      button.addEventListener('mouseenter', () => {
+        const dropdownId = button.getAttribute('data-dropdown-button-id');
+        const dropdownContent = document.querySelector(`[data-dropdown-id="${dropdownId}"]`);
+        dropdownContents.forEach((content) => {
+          if (content !== dropdownContent) {
+            content.classList.add('hidden');
+          }
+        });
+  
+          dropdownContent.classList.toggle('hidden');
+      });
+  
+      button.addEventListener('click', () => {
+        const dropdownId = button.getAttribute('data-dropdown-button-id');
+        const dropdownContent = document.querySelector(`[data-dropdown-id="${dropdownId}"]`);
+        dropdownContents.forEach((content) => {
+          if (content !== dropdownContent) {
+            content.classList.add('hidden');
+          }
+        });
+  
+          dropdownContent.classList.toggle('hidden');
+      });
+    });
+  
+    dropdownContents.forEach((content) => {
+      content.addEventListener('mouseleave', () => {
+        content.classList.add('hidden');
+      });
+    });
+  
+    document.addEventListener('click', (event) => {
+      if (!event.target.classList.contains('dropdown-button')) {
+        dropdownContents.forEach((content) => {
+          if (!content.contains(event.target)) {
+            content.classList.add('hidden');
+          }
+        });
+      }
+    });
+
+    // Mobile Nav Bar
+    const navMobileMenuButton = document.getElementById('navMobileMenuButton');
+    const navMobileMenu = document.getElementById('navMobileMenu');
+    const navMobileMenuButtonClose = document.getElementById('navMobileMenuButtonClose')
+
+    navMobileMenuButton.addEventListener('click', () => {
+        navMobileMenu.classList.remove('hidden');
+    });
+    navMobileMenuButtonClose.addEventListener('click', () => {
+        navMobileMenu.classList.add('hidden');
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!navMobileMenuButton.contains(event.target) 
+        && !navMobileMenu.contains(event.target)
+        && !navMobileMenuButtonClose.contains(event.target)
+        ) {
+            navMobileMenu.classList.add('hidden');
+        }
+    });
+
+    // Menu Nav Dropdown
+    const dropdownButtonsMobile = document.querySelectorAll('.dropdown-button-mobile');
+    const dropdownContentsMobile = document.querySelectorAll('.dropdown-content-mobile');
+     
+    dropdownButtonsMobile.forEach((button) => { 
+      button.addEventListener('click', () => {
+        const dropdownId = button.getAttribute('data-dropdown-mobile-button-id');
+        const dropdownContentMobile = document.querySelector(`[data-dropdown-mobile-id="${dropdownId}"]`);
+        dropdownContentsMobile.forEach((content) => {
+          console.log(dropdownId)
+          if (content !== dropdownContentMobile) {
+            console.log(dropdownContentMobile)
+            content.classList.add('hidden');
+          }
+        });
+          dropdownContentMobile.classList.toggle('hidden');
+      });
+    });
+      
+});
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        purple: '#3f3cbb',
+        midnight: '#121063',
+        metal: '#565584',
+        tahiti: '#3ab7bf',
+        silver: '#ecebff',
+        'bubble-gum': '#ff77e9',
+        bermuda: '#78dcca',
+      },
+    },
+  },
+  plugins: [],
+  safelist: [
+    {
+      pattern:
+        /(bg|text|border)-(transparent|current|white|purple|midnight|metal|tahiti|silver|bermuda)/,
+    },
+  ],
+}
+
