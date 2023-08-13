91e8f8a1c5f500a4583154ead462451040a9a9f6
unknown <karl.mullr@gmail.com>
2023-08-13 19:26:05 +0200

    search results

--- assets/search-bar.js
+++ assets/search-bar.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const openSearchBar = document.querySelectorAll('.button-search');
+    console.log(openSearchBar);
     const closeSearchBar = document.getElementById('close-search');
     const searchBar = document.getElementById('search-bar');
 
-    openSearchBar.addEventListener('click', () => {
-        searchBar.classList.remove('hidden');
-    })
+    openSearchBar.forEach(button => {
+        button.addEventListener('click', () => {
+            searchBar.classList.remove('hidden');
+        });
+    });
 
         closeSearchBar.addEventListener('click', () => {
         searchBar.classList.add('hidden');
