738c02b7eade5a7e2ef19908ae7aa4cc099aafd7
unknown <karl.mullr@gmail.com>
2023-08-15 11:46:16 +0200

    address form

--- assets/addressModal.js
+++ assets/addressModal.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const openAddressModal = document.getElementById('openAddressModal');
     const closeAddressModal = document.getElementById('closeAddressModal');
     const addressModal = document.getElementById('addressModal');
-
+    const modalContent = document.getElementById('modalContent');
     // ouverture sur le click
     openAddressModal.addEventListener('click', () => {
         addressModal.classList.remove('hidden');
@@ -12,5 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
         addressModal.classList.add('hidden');
     })
     // fermeture click en dehors
+    document.addEventListener('click', (event) => {
+        if (event.target === modalContent) {
+            addressModal.classList.add('hidden');
+        }
+    });
     // fermeture après submit
 })
